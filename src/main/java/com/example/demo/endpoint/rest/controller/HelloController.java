91b31a4febb7e9700985c6f28dc335ed43fbe4c2
Nantenaina Ramanatombosoa <127602403+Tombosoa@users.noreply.github.com>
2025-02-10 15:29:15 +0300

    Update HelloController.java

--- src/main/java/com/example/demo/endpoint/rest/controller/HelloController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.demo.endpoint.rest.controller.health;
+package com.example.demo.endpoint.rest.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
