732d17da12ce86dc75a226a688fae9a1c4ae6257
Nantenaina Ramanatombosoa <127602403+Tombosoa@users.noreply.github.com>
2025-02-10 15:31:08 +0300

    Update HelloController.java

--- src/main/java/com/example/demo/endpoint/rest/controller/HelloController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
-public class PingController {
+public class HelloController {
 
   public static final ResponseEntity<String> OK = new ResponseEntity<>("OK", HttpStatus.OK);
   public static final ResponseEntity<String> KO =
